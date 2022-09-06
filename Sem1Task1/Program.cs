7a6aa1617b81f3d20ebeb131b6ce3dba86ca1d31
olga <yavruyano90@mail.ru>
2022-09-06 11:02:50 +0300

    sem1task1

---
+++ Sem1Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿//============================================================================================================
+// # 1 Напишите пргограмму, которая на вход принимает два числа и проверяет,
+// является ли первое число квадратом второго
+//============================================================================================================
+
+String? inputLineA = Console.ReadLine();
+String? inputLineB = Console.ReadLine();
+if (inputLineA != null&&inputLineB != null)
+{
+    int inputNumberA = int.Parse(inputLineA);
+    int inputNumberB = int.Parse(inputLineB);
+
+    bool outResult = (inputNumberA * inputNumberA==inputNumberB);
+
+    Console.WriteLine(outResult);
+}
