a2fa19642324fc2b444884276592114a2237f41e
olga <yavruyano90@mail.ru>
2022-09-06 13:48:20 +0300

    sem1task3

---
+++ Sem1Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿// //============================================================================================================
+// # 2 Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее
+//============================================================================================================
+
+String? inputLineA = Console.ReadLine();
+String? inputLineB = Console.ReadLine();
+
+int inputNumberA = int.Parse(inputLineA);
+int inputNumberB = int.Parse(inputLineB);
+
+if (inputLineA != null && inputLineB != null)
+{
+    if (inputNumberA > inputNumberB)
+    {        
+        Console.Write("max = ");
+        Console.Write(inputNumberA);
+    }    
+    else
+    {       
+       Console.Write("max = ");
+       Console.Write(inputNumberB);
+    }
+}
