647bd8c529a39da91efec0c508345d5fa4672612
olga <yavruyano90@mail.ru>
2022-09-07 20:26:59 +0300

    task9

--- Sem2Task9/Program.cs
+++ Sem2Task9/Program.cs
@@ -5,6 +5,7 @@
 
 void MyVariant()
 {
+    Console.WriteLine( "Метод 1" );
     System.Random numberGenerator = new System.Random();
 
     int number = numberGenerator.Next(10, 100);
@@ -29,13 +30,34 @@ void MyVariant()
 void Variant()
 {
     // Вариант2
+    Console.WriteLine( "Метод 2" );
+    System.Random numberGenerator = new System.Random();
 
-    Console.WriteLine((firstDigit > secondDigit) ? firstDigit : secondDigit);
+    int number = numberGenerator.Next(10, 100);
+
+    Console.WriteLine(number);
+
+    int firstDigit = number / 10;
+    int secondDigit = number % 10;
+
+    if (firstDigit > secondDigit)
+
+        Console.WriteLine((firstDigit > secondDigit) ? firstDigit : secondDigit);
 }
 
 void charVariant()
 {
+
     // Вариант char
+    Console.WriteLine( "Метод 3" );
+    System.Random numberGenerator = new System.Random();
+
+    int number = numberGenerator.Next(10, 100);
+
+    Console.WriteLine(number);
+
     char[] charArray = number.ToString().ToCharArray();
     Console.WriteLine(charArray[0] > charArray[1] ? charArray[0] : charArray[1]);
 }
+
+MyVariant();
