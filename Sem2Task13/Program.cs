fad93fa234383a480ba527c4ecaf279d88ef3988
olga <yavruyano90@mail.ru>
2022-09-09 11:28:16 +0300

    task15

---
+++ Sem2Task13/Program.cs
@@ -0,0 +1,25 @@
+﻿//=============================================================================================
+// #13 Напишите программу, которая выводит третью цифру заданного числа или 
+// сообщает, что третьей цифры нет.
+// ============================================================================================
+
+
+Console.WriteLine("Введите число");
+String? inputLineB = Console.ReadLine();
+int inputNumberB = 0;
+int Lenght = 0;
+inputNumberB = int.Parse(inputLineB);
+
+if (inputNumberB.Lenght >= 3)
+{
+char[] charArray = inputNumberB.ToString().ToCharArray();
+Console.WriteLine(charArray[2]);
+}
+else 
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+
+
+
+
---
+++ Sem2Task15/Program.cs
@@ -0,0 +1,32 @@
+﻿//=============================================================================================
+// #13 Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным.
+// ============================================================================================
+
+
+
+String? inputLine = Console.ReadLine();
+
+if (inputLine != null)
+{
+    int inputdayofWeek = int.Parse(inputLine);
+    
+    string[] dayofWeek = new string[7];
+    dayofWeek[0] = "Понедельник";
+    dayofWeek[1] = "вторник";
+    dayofWeek[2] = "среда";
+    dayofWeek[3] = "четверг";
+    dayofWeek[4] = "пятница";
+    dayofWeek[5] = "суббота";
+    dayofWeek[6] = "воскресенье";
+
+if(inputdayofWeek == 6 || inputdayofWeek == 5)
+{
+    Console.WriteLine("Выходной");
+}
+else
+{
+    Console.WriteLine(dayofWeek[inputdayofWeek-1]);
+}
+    
+}
