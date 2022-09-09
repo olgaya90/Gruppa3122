90ed509e045df40a8180a294a66a10f35677f98c
olga <yavruyano90@mail.ru>
2022-09-09 17:03:16 +0300

    task13

--- Sem2Task13/Program.cs
+++ Sem2Task13/Program.cs
@@ -1,25 +1,34 @@
 ﻿//=============================================================================================
-// #13 Напишите программу, которая выводит третью цифру заданного числа или 
-// сообщает, что третьей цифры нет.
+// #13 Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 // ============================================================================================
 
+void Variant1()
+{
+    Console.WriteLine("Введите трехзначное число");
+    String? inputLineB = Console.ReadLine();
+    int inputNumberB = 0;
+    inputNumberB = int.Parse(inputLineB);
 
-Console.WriteLine("Введите число");
+    char[]
+    charArray = inputNumberB.ToString().ToCharArray();
+    Console.WriteLine(charArray [2]);
+}
+
+
+
+Console.WriteLine("Введите трехзначное число");
 String? inputLineB = Console.ReadLine();
 int inputNumberB = 0;
-int Lenght = 0;
 inputNumberB = int.Parse(inputLineB);
+int result  = 0;
+int leght = inputNumberB;
+while (leght >= 1000) leght = leght / 10;
+result = inputNumberB < 100 ? -1 : leght % 10;
 
-if (inputNumberB.Lenght >= 3)
-{
-char[] charArray = inputNumberB.ToString().ToCharArray();
-Console.WriteLine(charArray[2]);
-}
-else 
-{
-    Console.WriteLine("Третьей цифры нет");
-}
+Console.WriteLine(result != -1 ? result : "третьей цифры нет");
 
 
 
 
+
+//void Variant1();
