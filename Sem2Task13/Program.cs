fafc1c7173f87dd3e9757d822e03c9a9c820a4ae
olga <yavruyano90@mail.ru>
2022-09-09 19:28:37 +0300

    task13

--- Sem2Task13/Program.cs
+++ Sem2Task13/Program.cs
@@ -8,27 +8,28 @@ void Variant1()
     String? inputLineB = Console.ReadLine();
     int inputNumberB = 0;
     inputNumberB = int.Parse(inputLineB);
+    
+    char[] charArray = inputNumberB.ToString().ToCharArray();
+    Console.WriteLine(charArray.Length == 3 ? (charArray[2]) : ("Третьей цифры нет"));
+}
 
-    char[]
-    charArray = inputNumberB.ToString().ToCharArray();
-    Console.WriteLine(charArray [2]);
+
+void Variant2()
+{
+    Console.WriteLine("Введите трехзначное число");
+    String? inputLineB = Console.ReadLine();
+    int inputNumberB = 0;
+    inputNumberB = int.Parse(inputLineB);
+    int result = 0;
+    int leght = inputNumberB;
+    while (leght >= 1000) leght = leght / 10;
+    result = inputNumberB < 100 ? -1 : leght % 10;
+
+    Console.WriteLine(result != -1 ? result : "третьей цифры нет");
 }
 
 
 
-Console.WriteLine("Введите трехзначное число");
-String? inputLineB = Console.ReadLine();
-int inputNumberB = 0;
-inputNumberB = int.Parse(inputLineB);
-int result  = 0;
-int leght = inputNumberB;
-while (leght >= 1000) leght = leght / 10;
-result = inputNumberB < 100 ? -1 : leght % 10;
 
-Console.WriteLine(result != -1 ? result : "третьей цифры нет");
-
-
-
-
-
-//void Variant1();
+ Variant1();
+//Variant2();
