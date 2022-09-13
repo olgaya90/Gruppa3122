ab43e83d9dc4dbf7b1a6d16ff3dffadfee093541
olga <yavruyano90@mail.ru>
2022-09-13 13:08:40 +0300

    task20

---
+++ Sem3Task18/Program.cs
@@ -0,0 +1,39 @@
+﻿//===================================================================================================
+// Напишите программу, которая по заданному номеру
+// четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+//===================================================================================================
+//чтение данных из консоли
+int ReadData(string line)
+{
+    //выводим сообщение
+    Console.WriteLine(line);
+    //считываем число
+    int num = int.Parse(Console.ReadLine() ?? "0");
+    //возвращаем значение
+    return num;
+
+}
+string QuterBorderAsk(int numQuter)
+{
+    if (numQuter == 1)
+        return "x>0 y>0";
+    if (numQuter == 2)
+        return "x<0 y>0";
+    if (numQuter == 3)
+        return "x<0 y<0";
+    if (numQuter == 4)
+        return "x>0 y<0";
+    return "";
+}
+
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+int num = ReadData("введите номер четверти: ");
+
+
+string res = QuterBorderAsk(num);
+
+PrintResult(res);
---
+++ Sem3Task20/Program.cs
@@ -0,0 +1,33 @@
+﻿//===================================================================================================
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между
+// ними в 2D пространстве.
+//===================================================================================================
+//чтение данных из консоли
+int ReadData(string line)
+{
+    //выводим сообщение
+    Console.WriteLine(line);
+    //считываем число
+    int num = int.Parse(Console.ReadLine() ?? "0");
+    //возвращаем значение
+    return num;
+
+}
+double Calculate(int x1, int x2, int y1, int y2)
+{
+    return Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)); ;
+}
+
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+
+
+int x1 = ReadData("введите координаты x1:");
+int y1 = ReadData("введите координаты y1:");
+int x2 = ReadData("введите координаты x2:");
+int y2 = ReadData("введите координаты y2:");
+
+PrintResult(Math.Round(Calculate(x1, y1, x2, y2), 2).ToString());
