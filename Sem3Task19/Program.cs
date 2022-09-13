b8cb729cb871ed3f88c21ce2d4eab849ad66b179
olga <yavruyano90@mail.ru>
2022-09-13 15:24:10 +0300

    task19

--- Sem3Task19/Program.cs
+++ Sem3Task19/Program.cs
@@ -8,30 +8,46 @@ int ReadData(string line)
     //выводим сообщение
     Console.WriteLine(line);
     //считываем число
-    int numberP = int.Parse(Console.ReadLine() ?? "0");
+    int numberB = int.Parse(Console.ReadLine() ?? "0");
     //возвращаем значение
-    return numberP;
-    void PrintResult(string line)
-    {
-        Console.WriteLine(line);
-    }
+    return numberB;
+
+
 
 }
-bool TestPolin(int numberP)
+bool TestPolin(int numberB, int rev, int Count)
 {
+    rev = 0;
+    Count = 0;
+    while (numberB > 9999 && numberB < 100000)
+    {
+        numberB = numberB / 10;
+        rev = rev * 10 + numberB % 10;
+        Count++;
+    }
+    Console.WriteLine(Count);
+    if (numberB == rev)
+    {
+        Console.WriteLine("Число является палиндромом");
+    }
+    else
+    {
+        numberB = numberB * 10 + numberB % 10;
+        if (rev == numberB)
+            Console.WriteLine("Число является палиндромом");
+        else
+            Console.WriteLine("Число не является палиндромом");
 
-    if ((numberP / 10000 == numberP / 10) && (((numberP / 1000) % 10) == (numberP / 10) % 10));
-    {
-        return "Это слово является палиндромом";
-       
     }
-    if (numberP> 0);
-    
-    {
-        return "Это слово не является палиндромом";
-    }
+
+
+
 
 }
+Console.ReadLine();
+
+
+
 
 
 
