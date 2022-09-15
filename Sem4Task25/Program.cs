34a43463c0a5f7a4ec91c070ed8e87116b0c230f
olga <yavruyano90@mail.ru>
2022-09-15 19:56:04 +0300

    task25

---
+++ Sem4Task25/Program.cs
@@ -0,0 +1,39 @@
+﻿//=========================================================
+// #25 Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в
+// натуральную степень B.
+//=========================================================
+int ReadData(string line)
+{
+    //выводим сообщение
+    Console.Write(line);
+    //считываем число
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    //возвращаем значение
+    return number;
+
+}
+//метод вывода результата
+void PrintResult(long line)
+{
+    Console.WriteLine(line);
+}
+
+long Pow(int numA, int numB)
+{
+    long res = 1;
+    for (int i = 1; i <= numB; i++)
+    {
+        res = res * numA;
+    }
+    return res;
+}
+
+
+int numA = ReadData("Введите числа A: ");
+int numB = ReadData("Введите числа B: ");
+long res = Pow(numA, numB);
+Console.WriteLine(res);
+
+
+
+
