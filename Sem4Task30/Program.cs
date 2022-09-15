6c3ac645750282626b664a6c4eddabf4151eebfc
olga <yavruyano90@mail.ru>
2022-09-15 17:59:09 +0300

    task30

---
+++ Sem4Task30/Program.cs
@@ -0,0 +1,46 @@
+﻿//=====================================================================
+// Напишите программу, которая выводит массив из 8
+//элементов, заполненный нулями и единицами в случайном порядке.
+//=====================================================================
+int ReadData(string line)
+{
+    //выводим сообщение
+    Console.Write(line);
+    //считываем число
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    //возвращаем значение
+    return number;
+
+}
+
+
+int [] GenArray(int arrLen)
+{
+    int [] array = new int[arrLen];
+    Random ren = new Random();
+    for (int i=0; i<arrLen; i++)
+    {
+        array[i] = ren.Next(0,2);
+    }
+    return array;
+}
+
+//метод вывода результата
+void PrintArray(int [] array)
+{
+    for (int i=0; i<array.Length; i++)
+    {
+        Console.Write(array[i]+ ", ");
+    }
+    {
+        Console.WriteLine(array[array.Length-1]);
+    }
+}
+
+int arrLen = ReadData("Введите длину массива:");
+int[] array = GenArray(arrLen);
+PrintArray(array);
+
+PrintArray(GenArray(ReadData("Введите число ")));
+
+
