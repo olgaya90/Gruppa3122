d7f110ad2d94dded371aee705a261b9b8e6f05a8
olga <yavruyano90@mail.ru>
2022-09-15 17:57:29 +0300

    task26

---
+++ Sem4Task26/Program.cs
@@ -0,0 +1,36 @@
+﻿//================================================================
+//Напишите программу, которая принимает на вход
+//число и выдаёт количество цифр в числе.
+//=================================================================
+int ReadData(string line)
+{
+    //выводим сообщение
+    Console.Write(line);
+    //считываем число
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    //возвращаем значение
+    return number;
+
+}
+//метод вывода результата
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+
+int DigitCount(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        sum += 1;
+        num = num / 10;
+    }
+    return sum;
+}
+
+
+int number = ReadData("Введите число: ");
+int numberofDigit = DigitCount(number);
+PrintResult("Количество цифр в числе равно: " + numberofDigit);
